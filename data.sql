3e7442297849b9ec93b96b76b5f3aada5ada70a4
diamond-nicholas <diamondnicholas154@gmail.com>
2021-02-17 16:43:57 +0100

    Select the code that shows the population density of China, Australia, Nigeria and France

--- data.sql
+++ data.sql
@@ -28,3 +28,7 @@ SELECT name, area*2 FROM world WHERE population = 64000
 SELECT name, area, population
   FROM world
  WHERE area > 50000 AND population < 10000000
+
+SELECT name, population/area
+  FROM world
+ WHERE name IN ('China', 'Nigeria', 'France', 'Australia')
