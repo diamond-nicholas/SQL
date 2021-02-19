51dfe3026a6a0bb1cdfc9973ea5ad88a9a0941c0
diamond-nicholas <diamondnicholas154@gmail.com>
2021-02-19 21:19:12 +0100

    add the tutorial to the nested select

--- data.sql
+++ data.sql
@@ -105,6 +105,7 @@ SELECT name FROM world
  WHERE continent = 'South America'
    AND population > 40000000
 
+-- select from noble tutorial and quiz
 SELECT yr, subject, winner
   FROM nobel
  WHERE yr = 1950
@@ -186,3 +187,47 @@ SELECT DISTINCT yr
    FROM nobel 
   WHERE yr ='1960' 
   GROUP BY subject
+
+-- nested select tutorial
+
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia')
+
+SELECT name FROM world
+WHERE continent = 'Europe' AND
+gdp/population > (SELECT gdp/population FROM world
+                    WHERE name = 'United Kingdom')
+
+SELECT name, continent FROM world
+WHERE continent IN (SELECT continent FROM world
+                     WHERE name IN ('Argentina', 'Australia')) ORDER BY name;
+
+SELECT name, population FROM world
+WHERE population > (SELECT population FROM world
+                     WHERE name = 'Canada') AND population < (SELECT population FROM world
+                     WHERE name = 'Poland')
+
+SELECT name, CONCAT(ROUND(100*population/(SELECT population FROM world WHERE name = 'Germany')), '%')
+ FROM world 
+WHERE continent = 'Europe'
+
+SELECT name FROM world
+WHERE gdp > ALL(SELECT gdp FROM world
+               WHERE gdp > 0 AND continent = 'Europe')
+
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area>0)
+
+SELECT continent, name FROM world x WHERE name <= ALL(SELECT name FROM world y WHERE x.continent = y.continent);
+
+SELECT name, continent, population 
+FROM world x
+WHERE 25000000 > ALL (SELECT population FROM world y WHERE x.continent = y.continent);
+
+SELECT name, continent FROM world x
+  WHERE population > ALL(SELECT 3*population FROM world y WHERE x.continent = y.continent AND x.name <> y.name)
