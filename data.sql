7b8de4973970232b9fc9faf42c89a64002fda36e
diamond-nicholas <diamondnicholas154@gmail.com>
2021-02-19 14:54:43 +0100

    complete the select from nobel tutorial on sqlzoo

--- data.sql
+++ data.sql
@@ -104,3 +104,56 @@ SELECT name FROM world
 SELECT name FROM world
  WHERE continent = 'South America'
    AND population > 40000000
+
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
+
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
+
+SELECT yr, subject FROM nobel
+WHERE winner = 'Albert Einstein'
+
+SELECT winner
+  FROM nobel
+ WHERE yr >= 2000
+   AND subject = 'Peace'
+
+SELECT yr, subject, winner FROM nobel
+WHERE yr BETWEEN 1980 AND 1989
+AND subject = 'Literature'
+
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt','Woodrow Wilson','Jimmy Carter', 'Barack Obama')
+
+SELECT winner FROM nobel
+WHERE winner LIKE 'john%'
+
+SELECT yr, subject, winner FROM nobel
+WHERE yr = 1980 AND subject = 'Physics'
+XOR
+yr = 1984 AND subject = 'Chemistry'
+
+SELECT yr, subject, winner FROM nobel
+WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine')
+
+SELECT yr, subject, winner FROM nobel
+WHERE subject = 'Medicine'AND yr < 1910 XOR subject = 'Literature' AND yr >= 2004
+
+SELECT yr, subject, winner FROM nobel
+WHERE winner = 'PETER GRÜNBERG'
+
+SELECT yr, subject, winner FROM nobel
+WHERE winner = 'EUGENE O''NEILL'
+
+SELECT winner, yr, subject FROM nobel
+WHERE winner LIKE 'Sir%' ORDER BY yr DESC, winner
+
+SELECT winner, subject 
+  FROM nobel
+ WHERE yr=1984
+ ORDER BY subject IN ('Physics','Chemistry'), subject,winner
+
